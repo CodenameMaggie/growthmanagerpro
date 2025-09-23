d64613c2713867cbafa148c21a98279c9960aba4
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-09-23 15:09:25 -0700

    Update prospects.js

--- api/prospects.js
+++ api/prospects.js
@@ -1,9 +1,1 @@
-export default async function handler(req, res) {
-  res.json({ 
-    message: "API is working",
-    env_check: {
-      has_url: !!process.env.SUPABASE_URL,
-      has_key: !!process.env.SUPABASE_ANON_KEY
-    }
-  });
-}
+
