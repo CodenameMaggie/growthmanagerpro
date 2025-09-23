fa873a7d1bcdc2e912fb00f7d975df1464ba8ffa
CodenameMaggie <maggie@maggieforbesstrategies.com>
2025-09-23 07:11:39 -0700

    Update prospects.js

--- api/prospects.js
+++ api/prospects.js
@@ -1,4 +1,4 @@
-// api/prospects.js - Supabase Real-time Version
+// api/prospects.js - Supabase Real-time Version- updated version
 import { createClient } from '@supabase/supabase-js'
 
 // Initialize Supabase client
